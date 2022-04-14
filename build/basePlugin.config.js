5337fdc7249ffe5420fd4dc328792b84e05862c6
Aivaras Čiurlionis <aivaras@sneakybox-studios.com>
2022-04-14 14:46:05 +0300

    Exclude minificatino from npm bundle

--- build/basePlugin.config.js
+++ build/basePlugin.config.js
@@ -2,7 +2,6 @@ import commonjs from "@rollup/plugin-commonjs";
 import vue from "rollup-plugin-vue";
 import resolve from "rollup-plugin-node-resolve";
 import replace from "rollup-plugin-replace";
-import { uglify } from "rollup-plugin-uglify";
 import typescript from "rollup-plugin-typescript2";
 import babel from "@rollup/plugin-babel";
 import json from "@rollup/plugin-json";
@@ -41,7 +40,6 @@ export default {
     }),
     commonjs(),
     json(),
-    uglify(),
     replace({
       "process.env.NODE_ENV": JSON.stringify("production"),
     }),
