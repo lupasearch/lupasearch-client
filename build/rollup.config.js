b092bd4a6ff7ceabb1e4d275f76518ab6495bb4c
aivaras <aivaras@lupasearch.com>
2023-09-25 09:06:38 +0300

    Include chat css build

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,4 +1,5 @@
 import lupaContainerStyleConfig from './lupaContainerStyle.config'
 import lupaStyleConfig from './lupaContainerStyle.config'
+import lupaChatConfig from './lupaChatStyle.config'
 
-export default [lupaContainerStyleConfig, lupaStyleConfig]
+export default [lupaContainerStyleConfig, lupaStyleConfig, lupaChatConfig]
