576a20f297db69081e209cb826c7189efec5d9fc
aivaras <aivaras@lupasearch.com>
2024-08-09 12:14:06 +0300

    More labels, pagination fix

--- src/constants/searchResults.const.ts
+++ src/constants/searchResults.const.ts
@@ -40,7 +40,7 @@ export const DEFAULT_OPTIONS_RESULTS = {
         top: false,
         bottom: true
       },
-      sizes: [10, 20, 25, 50]
+      sizes: [12, 24, 36]
     },
     pageSelection: {
       position: {
